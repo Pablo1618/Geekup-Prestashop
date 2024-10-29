6e7999af720040af3dfe40e0499cd03fe63329d6
root <root@Komp.localdomain>
2024-10-29 19:28:34 +0100

    Changes: - finalized the repo setup, everything should now work properly with minimal effort

---
+++ prestashop/psdata/var/cache/prod/smarty/compile/37/d9/ec/37d9ec0471988ccf2910ade7c86c827a7eeff79d_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-10-29 18:47:22
+  from '/var/www/html/admin2137/themes/default/template/controllers/login/layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_67211faa603459_15763653',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '37d9ec0471988ccf2910ade7c86c827a7eeff79d' => 
+    array (
+      0 => '/var/www/html/admin2137/themes/default/template/controllers/login/layout.tpl',
+      1 => 1727103393,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67211faa603459_15763653 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
---
+++ prestashop/psdata/var/cache/prod/smarty/compile/76/ce/e2/76cee2bca4ad06772458801c864db6e3a1b44020_0.file.preload.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-10-29 18:47:44
+  from '/var/www/html/admin2137/themes/default/public/preload.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_67211fc09f6849_80885969',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '76cee2bca4ad06772458801c864db6e3a1b44020' => 
+    array (
+      0 => '/var/www/html/admin2137/themes/default/public/preload.tpl',
+      1 => 1727180379,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67211fc09f6849_80885969 (Smarty_Internal_Template $_smarty_tpl) {
+?><link
+      rel="preload"
+      href="<?php echo $_smarty_tpl->tpl_vars['admin_dir']->value;?>
+2d8017489da689caedc1.preload.woff2"
+      as="font"
+      crossorigin
+    ><?php }
+}
